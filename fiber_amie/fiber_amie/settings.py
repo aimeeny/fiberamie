4a3a2d0245be982b989d80ff692535aed057782c
Aimee <aimeenyy@gmail.com>
2022-06-21 10:33:49 -0700

    added requirements.txt

--- fiber_amie/fiber_amie/settings.py
+++ fiber_amie/fiber_amie/settings.py
@@ -160,3 +160,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
