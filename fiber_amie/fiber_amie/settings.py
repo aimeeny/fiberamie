fd5fe9b97475a8a3cdb1c41138ae7c2ef77aedac
Aimee <aimeenyy@gmail.com>
2022-06-21 11:48:55 -0700

    added gunicorn

--- fiber_amie/fiber_amie/settings.py
+++ fiber_amie/fiber_amie/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': env('DATABASE_USER'), d
+        'USER': env('DATABASE_USER'),
     }
 }
 
