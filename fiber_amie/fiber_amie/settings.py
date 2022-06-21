45c400e4edd5cd4a87f75b7ce0eccc10904593ba
Aimee <aimeenyy@gmail.com>
2022-06-21 11:34:50 -0700

    altered debug environment

--- fiber_amie/fiber_amie/settings.py
+++ fiber_amie/fiber_amie/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
